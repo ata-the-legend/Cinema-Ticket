35b694eea18254a99835010f568535de5b15b8a0
PouriyaMn <pouriyamnejad@gmail.com>
2023-05-26 20:17:00 +0330

    add 2 classmethod [create_account & show_account]

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -49,3 +49,52 @@ class BankAccount:
 
         psw = (str(uuid4().int))[:4]
         return psw
+
+    @classmethod
+    def create_account(cls,
+        input_owner_name: str,
+        input_bank: str,
+        input_balance: int) -> str:
+        """
+        create a bank account
+
+        Args:
+            input_owner_name (str)
+            input_bank (str)
+            input_balance (int)
+
+        Returns:
+            create a BankAccount instance 
+            and return its serial number as string
+        """
+        usr = cls(input_owner_name, input_bank, input_balance)
+        
+        return usr.serial_number
+        #this serial number should save in user_accounts!!!
+
+    @classmethod
+    def show_account(cls, serial_number: str) -> str:
+        """
+        show all information of each bank account
+
+        Args:
+            the bank account serial number as string
+
+        Returns:
+            str: show all information with an f"string"
+        """
+
+        info = cls.accounts_dict[serial_number]
+        owner_name = info["owner_name"]
+        bank = info["bank"]
+        serial_number = info["serial_number"]
+        cvv2 = info["cvv2"]
+        password = info["_BankAccount__password"]
+        balance = info["_BankAccount__balance"]
+
+        return f"\"{bank}\" bank information:\n\
+Owner Name    >>>   {owner_name}\n\
+Serial Number >>>   {serial_number}\n\
+CVV2          >>>   {cvv2}\n\
+Password      >>>   {password}\n\
+Balance       >>>   {balance:,}"
