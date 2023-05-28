b2371948b9239e11c8ae53f1e16ef7dde18a76b7
Ata Safapour <safapour.ata@gmail.com>
2023-05-28 04:38:02 +0330

    edit_user_access command added

--- user/create_staff.py
+++ user/create_staff.py
@@ -1,15 +1,17 @@
 #! /usr/bin/python3
 
 import click
-from user import User
+from user import User ,UserRole
 import pwinput
+from extra import get_object, delete, save
 
-@click.command()
+@click.group
 def mycommands():
     pass
 
+@click.command()
 @click.option('-u', '--username', prompt='Username', help='Define a username for new staff.')
-@click.option('-p', '--password', prompt=pwinput('Password'), help='Define a password for new staff.')
+@click.option('-p', '--password', prompt=('Password'), hide_input=True, help='Define a password for new staff.')
 @click.option('-b', '--birthdate', prompt='Birthdate', help='Birthdate of the new staff.')
 @click.option('-P', '--phone-number', help='Enter phone number of new staff.(optional)')
 def signup(username: str, password: str, birthdate: str, phone_number: str) -> None:
@@ -20,4 +22,50 @@ def signup(username: str, password: str, birthdate: str, phone_number: str) -> N
     User.create_user(username, password, birthdate, phone_number).promote_to_staff()
 
 
+@click.command()
+@click.argument('account_number')
+def cinema_bank_account(account_number: str) -> None:
+    '''
+    Bank account number for depositing the cinema income.
+    '''
+    #check the number -> not ok? raise error
+    #add a method in cinema module to take this
+    pass
 
+ACCESS = {
+    'D': 'Delete',
+    'p': 'Promote',
+    'd': 'Demote'
+}
+
+@click.command()
+@click.argument('username')
+@click.option('-a', '--access', type=click.Choice(ACCESS.keys()) , prompt='',help=f'{ACCESS}')
+def edit_user_access(username: str, access: str):
+    """
+    Change user access level or delete him.
+    """
+    user = get_object(username)
+    if user is None:
+        click.echo('Username not found!')
+    else:
+        match access:
+            case 'D':
+                delete(username)
+            case 'p':
+                user['user_role'] = UserRole.STAFF.value
+                delete(username)
+                save(user)
+            case 'd':
+                user['user_role'] = UserRole.PUBLIC.value
+                delete(username)
+                save(user)
+        click.echo(f'Successfully {ACCESS[access]}ed.')
+
+
+mycommands.add_command(signup)
+mycommands.add_command(cinema_bank_account)
+mycommands.add_command(edit_user_access)
+
+if __name__ == '__main__':
+    mycommands()
