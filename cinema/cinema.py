951d4b1b77d336a8fe1f121da9b9e2e42d52f213
Ramin <eslamiramin85@gmail.com>
2023-05-29 20:53:38 +0330

    modified cinema module and write orm for Movie class

--- cinema/cinema.py
+++ cinema/cinema.py
@@ -1,10 +1,11 @@
-from user import User
+# from user import User
 import uuid
+from extra import save_movie, get_movie_object, delete_movie, get_movie_database
 
 class Movie:
     def __init__(self, movie_id, name:str, director:str, duration_time:str, product_year,
                  age_limit, description:str = None):
-        self.id = movie_id
+        self.movie_id = movie_id
         self.name = name
         self.director = director
         self.description = description
@@ -12,190 +13,175 @@ class Movie:
         self.age_limit = age_limit
         self.product_year = product_year
 
-
-
     @classmethod
     def add_movie(cls,name:str, director:str, duration_time:str, product_year, age_limit, description:str=None):
         movie_id = cls.generate_id()
         movie = cls(movie_id, name, director, duration_time, product_year, age_limit, description)
-        # save (vars(movie))
+        save_movie(vars(movie))
 
     @classmethod
     def edit_movie(cls, movie_id, new_name,  new_director, new_duration_time, new_product_year,
                    new_age_limit, new_description):
-        #movie = get_object(movie_id)
-        #delete(movie_id)
+        movie = get_movie_object(movie_id)
+        delete_movie(movie_id)
         movie = cls(movie_id, new_name,  new_director, new_duration_time, new_product_year,
                    new_age_limit, new_description)
-        #save(vars(movie))
-
-
-
+        save_movie(vars(movie))
 
     @classmethod
     def delete_movie(cls, movie_id):
-        #delete(movie_id)
-
-
-
-
-
-
-
-
-
-
-    # def generate_id(self):
-    #     dict = get_database()
-    #     last_id = list(dict.key)[-1]
-    #     id_counter += 1
-    #     return str(id_counter)
-
-
-
-
-
-
-        ...
-
-
-class Cinema:
-    def __init__(self, name, location, working_hours, cinema_id):
-        self.name = name
-        self.location = location
-        self.working_hours = working_hours
-        self.cinema_id = cinema_id
-
-
-    @classmethod
-    def cinema_add(cls, name, location, working_hours):
-        cinema_id = cls.generate_id()
-        cinema = cls(name, location, working_hours, cinema_id)
-       #save(vars(cinema))
-
-    @classmethod
-    def cinema_edit(cls,cinema_id, new_name, new_location, new_working_hours):
-        # cinema = get_object(cinema_id)
-        # delete(cinema)
-        cinema = cls(new_name, new_location, new_working_hours,cinema_id)
-
-
-    @classmethod
-    def cinema_delete(cls, cinema_id):
-        #delete(cinema_id)
-
-
-
-
-
-    id_counter = 0
-
-    # def generate_id(self):
-    #     dict = get_database()
-    #     last_id = list(dict.key)[-1]
-    #     id_counter += 1
-    #     return str(id_counter)
-
-
-
-class Salon:
-    def __init__(self, name, seats_no,cinema_id, salon_id):
-        self.name =name
-        self.seats_no = seats_no
-        self.cinema_id = cinema_id
-        self.salon_id = salon_id
-
-
-    @classmethod
-    def add_salon(cls,name, seats_no, cinema_id):
-        salon_id = cls.generate_id()
-        salon = cls(name, seats_no, cinema_id, salon_id)
-        #save(vars(salon))
-
-
-    @classmethod
-    def edit_salon(cls, salon_id, new_name, new_seats_no, new_cinema_id):
-        #salon = get_object(salon_id)
-        #delete(salon_id)
-        salon = cls(new_name, new_seats_no, new_cinema_id, salon_id)
-        #save(vars(salon))
-
-    @classmethod
-    def delete_salon(cls,salon_id):
-        #delete(salon_id)
-
-    # def generate_id(self):
-    #     dict = get_database()
-    #     last_id = list(dict.key)[-1]
-    #     id_counter += 1
-    #     return str(id_counter)
-
-class session:
-    data_season = {}
-    user_reserve_season = {}
-    def __init__(self, session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime):
-        self.session_id = session_id
-        self.movie_id = movie_id
-        self.cinema_id = cinema_id
-        self.salon_id = salon_id
-        self.start_time = start_time
-        self.end_time = end_time
-        self.price = price
-        self.datetime = datetime
-
-
-    @classmethod
-    def add_session(cls, session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime):
-         session_id = cls.generate_id()
-         session = cls( session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime)
-        #save(vars(session))
-
-    @classmethod
-    def edit_session(cls, session_id, new_movie_id, new_cinema_id, new_salon_id, new_start_time, new_end_time,
-                     new_price, new_datetime):
-        #session = get_object(session_id)
-        #delete(session_id)
-         session = cls(session_id,new_movie_id, new_cinema_id, new_salon_id, new_start_time, new_end_time,
-                     new_price, new_datetime)
-        #save(vars(session)
-
-    @classmethod
-    def delete_session(cls, session_id):
-        #datetime(session_id)
-
-    # def generate_id(self):
-    #     dict = get_database()
-    #     last_id = list(dict.key)[-1]
-    #     id_counter += 1
-    #     return str(id_counter)
-
-
-
-
-
-
-
-
-
-
-class ticket :
-
-    def __int__(self, session_id, username_owner):
-        self.session_id = session_id
-        self.username_owner = username_owner
-
-
-    def buy_ticket(self, username_owner ,session_id, ):
+        # delete(movie_id)
         pass
 
+    @staticmethod
+    def generate_id():
+        dicti = get_movie_database()
+        try:
+            last_id = int(list(dicti.keys())[-1])
+            last_id += 1
+        except:
+            last_id = 1
+        return str(last_id)
 
+Movie.add_movie('Zakhm kari 2','hasan fathi', '2:00', '1402', '18','description')
+Movie.add_movie('Zakhm kari 1','hasan fathi', '2:00', '1402', '18','description')
+Movie.add_movie('Zakhm kari 3','hasan fathi', '2:00', '1402', '18','description')
 
-
-
-
-
-
-
+# class Cinema:
+#     def __init__(self, name, location, working_hours, cinema_id):
+#         self.name = name
+#         self.location = location
+#         self.working_hours = working_hours
+#         self.cinema_id = cinema_id
+#
+#
+#     @classmethod
+#     def cinema_add(cls, name, location, working_hours):
+#         cinema_id = cls.generate_id()
+#         cinema = cls(name, location, working_hours, cinema_id)
+#        #save(vars(cinema))
+#
+#     @classmethod
+#     def cinema_edit(cls,cinema_id, new_name, new_location, new_working_hours):
+#         # cinema = get_object(cinema_id)
+#         # delete(cinema)
+#         cinema = cls(new_name, new_location, new_working_hours,cinema_id)
+#
+#
+#     @classmethod
+#     def cinema_delete(cls, cinema_id):
+#         #delete(cinema_id)
+#         pass
+#
+#
+#
+#     # def generate_id(self):
+#     #     dict = get_database()
+#     #     last_id = list(dict.key)[-1]
+#     #     id_counter += 1
+#     #     return str(id_counter)
+#
+#
+#
+# class Salon:
+#     def __init__(self, name, seats_no,cinema_id, salon_id):
+#         self.name =name
+#         self.seats_no = seats_no
+#         self.cinema_id = cinema_id
+#         self.salon_id = salon_id
+#
+#
+#     @classmethod
+#     def add_salon(cls,name, seats_no, cinema_id):
+#         salon_id = cls.generate_id()
+#         salon = cls(name, seats_no, cinema_id, salon_id)
+#         #save(vars(salon))
+#
+#
+#     @classmethod
+#     def edit_salon(cls, salon_id, new_name, new_seats_no, new_cinema_id):
+#         #salon = get_object(salon_id)
+#         #delete(salon_id)
+#         salon = cls(new_name, new_seats_no, new_cinema_id, salon_id)
+#         #save(vars(salon))
+#
+#     @classmethod
+#     def delete_salon(cls,salon_id):
+#         #delete(salon_id)
+#         pass
+#
+#     # def generate_id(self):
+#     #     dict = get_database()
+#     #     last_id = list(dict.key)[-1]
+#     #     id_counter += 1
+#     #     return str(id_counter)
+#
+# class session:
+#     data_season = {}
+#     user_reserve_season = {}
+#     def __init__(self, session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime):
+#         self.session_id = session_id
+#         self.movie_id = movie_id
+#         self.cinema_id = cinema_id
+#         self.salon_id = salon_id
+#         self.start_time = start_time
+#         self.end_time = end_time
+#         self.price = price
+#         self.datetime = datetime
+#
+#
+#     @classmethod
+#     def add_session(cls, session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime):
+#          session_id = cls.generate_id()
+#          session = cls( session_id, movie_id, cinema_id, salon_id, start_time, end_time, price, datetime)
+#         #save(vars(session))
+#
+#     @classmethod
+#     def edit_session(cls, session_id, new_movie_id, new_cinema_id, new_salon_id, new_start_time, new_end_time,
+#                      new_price, new_datetime):
+#         #session = get_object(session_id)
+#         #delete(session_id)
+#          session = cls(session_id,new_movie_id, new_cinema_id, new_salon_id, new_start_time, new_end_time,
+#                      new_price, new_datetime)
+#         #save(vars(session)
+#
+#     @classmethod
+#     def delete_session(cls, session_id):
+#         #datetime(session_id)
+#
+#     # def generate_id(self):
+#     #     dict = get_database()
+#     #     last_id = list(dict.key)[-1]
+#     #     id_counter += 1
+#     #     return str(id_counter)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# class ticket :
+#
+#     def __int__(self, session_id, username_owner):
+#         self.session_id = session_id
+#         self.username_owner = username_owner
+#
+#
+#     def buy_ticket(self, username_owner ,session_id, ):
+#         pass
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 
--- cinema/extra.py
+++ cinema/extra.py
@@ -1,7 +1,8 @@
 import json
 
+#-----------------------------------------Cinema------------------------------------------
 
-def get_database() -> dict:
+def get_cinema_database() -> dict:
     """
     gets database content
     :return: dictionary of user accounts
@@ -14,13 +15,13 @@ def get_database() -> dict:
         print('You have error in get cinema-database', ex)
 
 
-def save(season: dict) -> None:
+def save_cinema(season: dict) -> None:
     """
     save object in database
     :param season: user object
     :return: None
     """
-    dic = get_database()
+    dic = get_cinema_database()
     season_name = season['season_name']
     dic.update({season_name: season})
     try:
@@ -30,13 +31,13 @@ def save(season: dict) -> None:
         print('You have error', ex)
 
 
-def delete(season_name: str) -> None:
+def delete_cinema(season_name: str) -> None:
     """
     delete user object from database
     :param season_name: username of user account
     :return: None
     """
-    dic = get_database()
+    dic = get_cinema_database()
     del dic[season_name]
     try:
         with open("cinema.json", "w") as fp:
@@ -45,7 +46,7 @@ def delete(season_name: str) -> None:
         print('You have error', ex)
 
 
-def get_object(season_name: str) -> dict | None:
+def get_cinema_object(season_name: str) -> dict | None:
     """
     get object from database
     :param season_name: username
@@ -60,4 +61,154 @@ def get_object(season_name: str) -> dict | None:
     except Exception:
         return None
 
+#-----------------------------------------Movie------------------------------------------
 
+
+def get_movie_database() -> dict:
+    try:
+        with open("movie.json", "r") as fp:
+            # Load the dictionary from the file
+            return json.load(fp)
+    except Exception as ex:
+        print('You have error in get cinema-database', ex)
+
+
+def save_movie(movie: dict) -> None:
+    dic = get_movie_database()
+    movie_id = movie['movie_id']
+    dic.update({movie_id: movie})
+    try:
+        with open("movie.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def delete_movie(movie_id: str) -> None:
+    dic = get_movie_database()
+    del dic[movie_id]
+    try:
+        with open("movie.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def get_movie_object(movie_id: str) -> dict | None:
+    """
+    get object from database
+    :param movie_id: username
+    :return: user object
+    """
+    try:
+        with open("movie.json", "r") as fp:
+            # Load the dictionary from the file
+            movie_dict = json.load(fp)
+            movie = movie_dict[movie_id]
+            return movie
+    except Exception:
+        return None
+
+
+#-----------------------------------------Salon------------------------------------------
+
+
+def get_salon_database() -> dict:
+    try:
+        with open("cinema.json", "r") as fp:
+            # Load the dictionary from the file
+            return json.load(fp)
+    except Exception as ex:
+        print('You have error in get cinema-database', ex)
+
+
+def save_salon(season: dict) -> None:
+    dic = get_salon_database()
+    season_name = season['season_name']
+    dic.update({season_name: season})
+    try:
+        with open("cinema.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def delete_salon(season_name: str) -> None:
+    dic = get_salon_database()
+    del dic[season_name]
+    try:
+        with open("cinema.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def get_salon_object(season_name: str) -> dict | None:
+    try:
+        with open("cinema.json", "r") as fp:
+            # Load the dictionary from the file
+            season_dict = json.load(fp)
+            season = season_dict[season_name]
+            return season
+    except Exception:
+        return None
+
+
+def get_session_database() -> dict:
+    """
+    gets database content
+    :return: dictionary of user accounts
+    """
+    try:
+        with open("cinema.json", "r") as fp:
+            # Load the dictionary from the file
+            return json.load(fp)
+    except Exception as ex:
+        print('You have error in get cinema-database', ex)
+
+
+def save_session(season: dict) -> None:
+    """
+    save object in database
+    :param season: user object
+    :return: None
+    """
+    dic = get_session_database()
+    season_name = season['season_name']
+    dic.update({season_name: season})
+    try:
+        with open("cinema.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def delete_session(season_name: str) -> None:
+    """
+    delete user object from database
+    :param season_name: username of user account
+    :return: None
+    """
+    dic = get_session_database()
+    del dic[season_name]
+    try:
+        with open("cinema.json", "w") as fp:
+            json.dump(dic, fp, indent=4)  # encode dict into JSON
+    except Exception as ex:
+        print('You have error', ex)
+
+
+def get_session_object(season_name: str) -> dict | None:
+    """
+    get object from database
+    :param season_name: username
+    :return: user object
+    """
+    try:
+        with open("cinema.json", "r") as fp:
+            # Load the dictionary from the file
+            season_dict = json.load(fp)
+            season = season_dict[season_name]
+            return season
+    except Exception:
+        return None
