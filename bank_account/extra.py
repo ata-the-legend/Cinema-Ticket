933344af8bba072edc459b47620840155353fb69
Ata Safapour <safapour.ata@gmail.com>
2023-05-31 06:32:46 +0330

    'Bug fixed'
    'fixed wrong key in bank extra'

--- bank_account/extra.py
+++ bank_account/extra.py
@@ -21,8 +21,8 @@ def save(bank_account: dict) -> None:
     :return: None
     """
     dic = get_database()
-    serial = bank_account['serial']
-    dic.update({serial: bank_account})
+    serial_number = bank_account['serial_number']
+    dic.update({serial_number: bank_account})
     try:
         with open("bank_account.py", "w") as fp:
             json.dump(dic, fp, indent=4)  # encode dict into JSON
@@ -30,14 +30,14 @@ def save(bank_account: dict) -> None:
         print('You have error', ex)
 
 
-def delete(serial: str) -> None:
+def delete(serial_number: str) -> None:
     """
     delete user object from database
-    :param serial: username of user account
+    :param serial_number: username of user account
     :return: None
     """
     dic = get_database()
-    del dic[serial]
+    del dic[serial_number]
     try:
         with open("bank_accounts.json", "w") as fp:
             json.dump(dic, fp, indent=4)  # encode dict into JSON
@@ -45,17 +45,17 @@ def delete(serial: str) -> None:
         print('You have error', ex)
 
 
-def get_object(serial: str) -> dict | None:
+def get_object(serial_number: str) -> dict | None:
     """
     get object from database
-    :param serial: username
+    :param serial_number: username
     :return: user object
     """
     try:
         with open("bank_accounts.json", "r") as fp:
             # Load the dictionary from the file
             bank_accounts = json.load(fp)
-            bank_account = bank_accounts[serial]
+            bank_account = bank_accounts[serial_number]
             return bank_account
     except Exception:
         return None
